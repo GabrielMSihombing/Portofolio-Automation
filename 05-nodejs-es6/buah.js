3447dd56440e97c378d8cdcd86e86da97c481ce8
GabrielMSihombing <gabrielmsihombing@gmail.com>
2024-09-13 20:08:44 +0700

    first try task day 5

---
+++ 05-nodejs-es6/buah.js
@@ -0,0 +1,20 @@
+let buah = [
+    {
+        nama: "Mangga",
+        warna: "Hijau",
+        rasa: "Manis",
+    },
+    {
+        nama: "Apel",
+        warna: "Merah",
+        rasa: "manis",
+    },
+    {
+        nama: "Jeruk",
+        warna: "Orange",
+        rasa: "Asam",
+    }
+    ]
+    
+export{buah}
+    
---
+++ 05-nodejs-es6/index.js
@@ -0,0 +1,6 @@
+import { buah } from "./buah.js";
+
+
+for(let buahh of buah){
+    console.log(buahh.nama);
+}

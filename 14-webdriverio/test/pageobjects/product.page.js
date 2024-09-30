6ddeab4f15d6d010d9fc1da8d3b31d51cbd62336
GabrielMSihombing <gabrielmsihombing@gmail.com>
2024-10-01 01:42:37 +0700

    task day 14

---
+++ 14-webdriverio/test/pageobjects/product.page.js
@@ -0,0 +1,105 @@
+import { $ } from "@wdio/globals";
+class productPage{
+
+    get nicknameInput(){return $('#nickname_field')}
+    get summaryInput(){return $('#summary_field')}
+    get reviewInput(){return $('#review_field')}
+    // get submitBtn(){return $('.action.submit.primary')}
+    get nickerrorMessage(){return $('#nickname_field-error')}
+    get sumerrorMessage(){return $('#summary_field-error')}
+    get reviewerrorMessage(){return $('#review_field-error')}
+    get errorMessageRating(){return $('#ratings[4]-error')}
+    get successMessage(){return $('.messages')}
+    get custReviewTitle(){return $('#customer-reviews')}
+    get inputQty (){return $('.input-text.qty')}
+    get errorMessageQty (){return $('#qty-error')}
+    get errorMsgQty(){return $('.messages')}
+    get priceCartElement (){return $('.amount')}
+    get image(){return $('.fotorama__img')}
+    // get changeImage (){return $('img.fotorama__img[src*="mb02-blue-0.jpg"]:first-of-type')}
+    get zoomInImage (){return $('.fotorama__nav__frame fotorama__nav__frame--fotorama__zoom-in zoom-in-loaded')}
+    get validateZoomInImage (){return $('.fotorama__stage__shaft')}
+
+    
+  
+    async openPage (){
+        await browser.url('https://magento.softwaretestingboard.com/fusion-backpack.html')
+        await browser.maximizeWindow()
+    }
+
+    async reviewPage(){
+      const reviewButton = await $('#tab-label-reviews-title')
+      await reviewButton.click()
+    }
+
+    async optionRatingByValueRating1() {
+        const addReviewButton = await $('a.action.add')
+        await addReviewButton.click()
+        // await addReviewButton.click()
+        await browser.execute(function () {
+            document.querySelector('#Rating_1').checked = true
+            
+        })
+    }
+    async optionRatingByValueRating5() {
+        const addReviewButton = await $('a.action.add')
+        await addReviewButton.click()
+        // await addReviewButton.click()
+        await browser.execute(function () {
+            document.querySelector('#Rating_5').checked = true
+            
+        })
+    }
+    async submitButton() {
+        const submitButtonOnly = await $('.action.submit.primary').click()
+    }
+    
+    //review page action
+    async reviewProcess(nickname, summary, review) {
+        await this.nicknameInput.setValue(nickname)
+        await this.summaryInput.setValue(summary)
+        await this.reviewInput.setValue(review)
+        // await this.submitBtn.click()
+    
+   }
+   async qtyProcess(qty) {
+    await this.inputQty.setValue(qty)
+   }
+   async addToCartButton() {
+        const addCartBtn = await $('#product-addtocart-button')
+        await addCartBtn.click()
+   }
+   async addReview() {
+        const addreview = await $('a.action.add')
+        await addreview.click()
+   }
+   async amountReview() {
+        const addreview = await $('a.action.view')
+        await addreview.click()
+    }
+   async zoomInOnProduct() {
+        const zoomInButton = $('.fotorama__zoom-in[data-gallery-role="fotorama__zoom-in"]');
+
+        await zoomInButton.click();
+    }
+   async zoomOutOnProduct() {
+        const zoomOutButton = $('.fotorama__zoom-out[data-gallery-role="fotorama__zoom-out"]');
+        await zoomOutButton.click();
+   
+    }
+    async priceProductElement() {
+        const totalPriceProduct = await $('.amount[data-th="Order Total"]')
+
+    }
+    async cart() {
+        const CartPage = $('.action.showcart[href="https://magento.softwaretestingboard.com/checkout/cart/"]');
+        await CartPage.click();
+   
+    }
+    async editViewcart() {
+        const viewCartPage = $('.action.viewcart');
+        await viewCartPage.click();
+    }
+
+}
+export default new productPage()
---
+++ 14-webdriverio/test/specs/product-details.spec.js
@@ -0,0 +1,194 @@
+import { browser, $, expect } from "@wdio/globals";
+import productPage from "../pageobjects/product.page.js";
+
+
+
+describe('FITUR REVIEW PRODUK', function (){
+    before('Cek Click Fusion Backpack Page', async function () {
+                await productPage.openPage()
+                
+                
+            })
+            it('menekan tab review produk', async function () {
+                await productPage.reviewPage()
+
+                await expect(productPage.custReviewTitle).toHaveText(
+                expect.stringContaining('Customer Reviews'))
+   
+            })
+            it('menekan tombol add review', async function () {
+
+                await productPage.addReview()
+
+                await expect(productPage.custReviewTitle).toHaveText(
+                expect.stringContaining('Customer Reviews'))
+
+                await browser.pause(7000)
+            
+            })
+           it('menekan tombol amount review', async function () {
+
+                await productPage.amountReview()
+
+                await expect(productPage.custReviewTitle).toHaveText(
+                expect.stringContaining('Customer Reviews'))
+
+                await browser.pause(7000)
+        
+            })
+            it('Mengecek response jika mengisi semua field rating 1 dan menekan tombol submit', async function () {
+                  
+                await productPage.optionRatingByValueRating1()
+                await browser.pause(5000);
+                await productPage.reviewProcess('tanoto','testing Fusion','testing Bagus')
+                await productPage.submitButton()
+               
+                await expect(productPage.successMessage).toHaveText(
+                expect.stringContaining('You submitted your review for moderation.'))
+
+                await browser.pause(5000)
+            })
+            it('Mengecek response jika mengisi semua field dan rating bintang 5', async function () {
+                
+                await productPage.optionRatingByValueRating5()
+                await browser.pause(1000);
+                await productPage.reviewProcess('tanot2','testing Fusion','testing Bagus')
+                await productPage.submitButton()
+
+                await expect(productPage.successMessage).toHaveText(
+                expect.stringContaining('You submitted your review for moderation.'))
+            })       
+            it('Mengecek response jika mengosongkan field input bintang', async function () {
+            
+                await productPage.addReview()
+                await browser.pause(2000);
+                await productPage.reviewProcess('testing','testing Fusion','testing Bagus')
+                await productPage.submitButton()
+
+                //belum cek validasi
+                // await expect(productPage.errorMessageRating).toHaveText(
+                // expect.stringContaining('Please select one of each of the ratings above.'))
+
+            })
+            it('mengirim review dengan mengisi semua input kecuali nickname', async function () {
+            
+                await productPage.optionRatingByValueRating5()
+                await browser.pause(10000)
+                await productPage.reviewProcess('','testing Fusion','testing Bagus')
+                await productPage.submitButton()
+
+                await expect(productPage.nickerrorMessage).toHaveText(
+                expect.stringContaining('This is a required field.'))
+            })
+           it('mengirim review dengan mengisi semua input kecuali summary', async function () {
+            
+                
+                await productPage.optionRatingByValueRating5()
+                await browser.pause(10000)
+                await productPage.reviewProcess('testing','','testing Bagus')
+                await productPage.submitButton()
+
+                await expect(productPage.sumerrorMessage).toHaveText(
+                expect.stringContaining('This is a required field.'))
+            })
+            it('mengirim review dengan mengisi semua input kecuali review', async function () {
+            
+                await browser.refresh()
+                await productPage.optionRatingByValueRating5()
+                await browser.pause(10000)
+                await productPage.reviewProcess('testing','testing Fusion','')
+                await productPage.submitButton()
+
+                await expect(productPage.reviewerrorMessage).toHaveText(
+                expect.stringContaining('This is a required field.'))
+            })
+           it('Mengecek response jika langsung menekan tombol submit', async function () {
+
+                await productPage.reviewProcess('','','')
+                await browser.pause(10000)
+                await productPage.submitButton()
+
+                await expect(productPage.reviewerrorMessage).toHaveText(
+                expect.stringContaining('This is a required field.'))
+            })
+           it('mengecek response jika menekan tombol add to cart', async function () {
+            
+                await productPage.addToCartButton()
+
+                await expect(productPage.successMessage).toHaveText(
+                expect.stringContaining('You added Fusion Backpack to your shopping cart.'))
+
+           })
+           it('mengecek response jika menekan tombol add to cart dengan jumlah barang 0', async function () {
+
+                await productPage.qtyProcess(0)
+                await productPage.addToCartButton()
+            
+                await expect(productPage.errorMessageQty).toHaveText('Please enter a quantity greater than 0.')
+                
+                await browser.pause(7000)
+           })
+           it('mengecek response jika menekan tombol add to cart dengan jumlah barang -1', async function () {
+
+                await productPage.qtyProcess(-1)
+                await productPage.addToCartButton()
+            
+                await expect(productPage.errorMessageQty).toHaveText('Please enter a quantity greater than 0.')
+                
+                await browser.pause(7000)
+           })
+           it('mengecek response jika menekan tombol add to cart dengan jumlah barang 99999', async function () {
+
+                await productPage.qtyProcess(99999)
+                await productPage.addToCartButton()
+
+                await expect(productPage.errorMessageQty).toHaveText('The maximum you may purchase is 10000.')
+
+                await browser.pause(7000)
+           })
+           it('mengecek response jika menekan tombol add to cart dengan jumlah barang 10000', async function () {
+
+                await productPage.qtyProcess(10000)
+                await productPage.addToCartButton()
+
+                await browser.pause(3000)
+
+                await expect(productPage.errorMsgQty).toHaveText('The requested qty exceeds the maximum qty allowed in shopping cart')
+
+                await browser.pause(7000)
+           })
+
+           it('meperbesar gambar produk', async function () {
+
+                await productPage.image.click()
+                //belum cek validasi
+                await browser.pause(7000)
+           })
+           it('Zoom In gambar produk', async function () {
+
+                await productPage.zoomInOnProduct()
+                //belum cek validasi
+                await browser.pause(7000)
+           })
+           it('Zoom Out gambar produk', async function () {
+
+                await productPage.zoomOutOnProduct()
+                //belum cek validasi
+                await browser.pause(7000)
+           })
+        //    it('mengecek jumlah yang diinputkan kedalam cart sama dengan isi cart menggunakan subtotal', async function () {
+
+        //         await productPage.qtyProcess(2)
+        //         await productPage.addToCartButton()
+        //         await browser.pause(3000)
+        //         await productPage.cart()
+        //         await productPage.editViewcart()
+
+        //         //belum cek validasi
+        //         // await expect(productPage.priceCartElement).toHaveText(
+        //         //     expect.stringContaining('$118.00'))
+
+        //         await browser.pause(7000)
+        //    })
+           
+        })
